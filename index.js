f142152856d5757eb741fc885207d78c42513307
lbeltrao <lucas.a.beltrao@capgemini.com>
2022-05-28 14:37:07 -0300

    second

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ var exec = require('child_process').exec;
   // the assigned public url for your tunnel
   // i.e. https://abcdefgjhij.localtunnel.me
   tunnel.url;
+  console.log(tunnel.url);
 
+  console.log("passo1");
   exec('rm redirectToLocal/redirect.js');
 
+  console.log("passo2");
   exec('echo "var redirectUrl = \''+ tunnel.url +'\' >> redirectToLocal/redirect.js');
 
+  console.log("passo3");
   exec('cd redirectToLocal && git add --all && git commit -m "'+(new Date().valueOf())+'"  && git push origin master')
+
+  console.log("fim");
 
   tunnel.on('close', () => {
     // tunnels are closed
