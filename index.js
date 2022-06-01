eff667f45987a4b2add5d68c5312c9e7d6039edf
lbeltrao <lucas.a.beltrao@capgemini.com>
2022-06-01 15:54:59 -0300

    ajuste gerais

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const localtunnel = require('localtunnel');
 var exec = require('child_process').exec;
 
 (async () => {
-  const tunnel = await localtunnel({ port: 80 });
+  const tunnel = await localtunnel({ port: 80, local_host: "drupal9.docksal" });
 
   // the assigned public url for your tunnel
   // i.e. https://abcdefgjhij.localtunnel.me
@@ -12,11 +12,15 @@ var exec = require('child_process').exec;
   console.log("passo1");
   exec('rm redirectToLocal/redirect.js');
 
-  console.log("passo2");
-  exec('echo "var redirectUrl = \''+ tunnel.url +'\' >> redirectToLocal/redirect.js');
+  console.log('echo "var redirectUrl = \''+ tunnel.url +'\'" >> redirectToLocal/redirect.js');
+  exec('echo "var redirectUrl = \''+ tunnel.url +'\'" >> redirectToLocal/redirect.js');
 
-  console.log("passo3");
-  exec('cd redirectToLocal && git add --all && git commit -m "'+(new Date().valueOf())+'"  && git push origin master')
+  console.log('cd redirectToLocal && git add --all && git commit -m "'+(new Date().valueOf())+'"  && git push origin master');
+  coffeeProcess = exec('cd redirectToLocal && git add --all && git commit -m "'+(new Date().valueOf())+'"  && git push origin master')
+
+  coffeeProcess.stdout.on('data', function(data) {
+    console.log(data); 
+  });
 
   console.log("fim");
 
